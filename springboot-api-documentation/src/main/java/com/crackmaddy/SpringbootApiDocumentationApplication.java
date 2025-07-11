90cede1cfec7b277c50f79a289eee7a96a621464
CrackMaddy <luv4code.spring@gmail.com>
2025-07-11 12:15:11 +0530

    initial commit

---
+++ springboot-api-documentation/src/main/java/com/crackmaddy/SpringbootApiDocumentationApplication.java
@@ -0,0 +1,13 @@
+package com.crackmaddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiDocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiDocumentationApplication.class, args);
+	}
+
+}
---
+++ springboot-api-documentation/src/test/java/com/crackmaddy/SpringbootApiDocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crackmaddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiDocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

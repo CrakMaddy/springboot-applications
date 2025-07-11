787f3d6ff585245ace07f4126be0c2a618830b69
CrackMaddy <luv4code.spring@gmail.com>
2025-07-11 12:39:43 +0530

    added dto classes

---
+++ springboot-api-documentation/src/main/java/com/crackmaddy/dto/ResponseDto.java
@@ -0,0 +1,11 @@
+package com.crackmaddy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+    private String statusCode;
+    private String statusMessage;
+}
